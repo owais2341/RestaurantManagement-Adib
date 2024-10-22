015dfe4e21f2bd478c1d68086e43e89749e90119
Syed Muhammad Owais Ali <smowais.ali@systemsltd.com>
2024-10-22 18:32:48 +0500

    Initial commit

--- src/main/java/com/JavaProj/Handler/AddReservationHandler .java
+++ src/main/java/com/JavaProj/Handler/AddReservationHandler .java
@@ -34,7 +34,9 @@ class AddReservationHandler implements HttpHandler {
             String dateTimeStr = jsonRequest.optString("dateTime");
             LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
 
-            // Call addReservation with all required parameters
+            // Call addReservation with all required parame
+                        // Call addReservation with all required parame
+
             String response = reservationManager.addReservation(restaurantId, name, email, phone, partySize, dateTime);
             exchange.sendResponseHeaders(200, response.getBytes().length);
             try (OutputStream os = exchange.getResponseBody()) {
