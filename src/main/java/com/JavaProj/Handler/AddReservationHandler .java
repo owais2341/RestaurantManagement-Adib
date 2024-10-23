03b4a4b2103bcadf611c49353fd18175c646a2e4
Syed Muhammad Owais Ali <smowais.ali@systemsltd.com>
2024-10-23 18:15:57 +0500

    Initial commit

--- src/main/java/com/JavaProj/Handler/AddReservationHandler .java
+++ src/main/java/com/JavaProj/Handler/AddReservationHandler .java
@@ -29,7 +29,7 @@ class AddReservationHandler implements HttpHandler {
             String restaurantId = jsonRequest.optString("restaurantId");
             String name = jsonRequest.optString("name");
             String email = jsonRequest.optString("email");
-            String phone = jsonRequest.optString("phone"); // Ensure you get the 
+            String phone = jsonRequest.optString("phone"); // Ensure you get 
             int partySize = jsonRequest.optInt("partySize");
             String dateTimeStr = jsonRequest.optString("dateTime");
             LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
